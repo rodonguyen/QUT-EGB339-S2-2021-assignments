223a1e5a643347fe5cc717f776b014d84496d55d
Rodo <rodonguyendd@gmail.com>
2021-08-28 18:26:12 +1000

    3

--- Assignment INDIVIDUAL/Assignment3/coloured_objects.m
+++ Assignment INDIVIDUAL/Assignment3/coloured_objects.m
@@ -4,19 +4,21 @@ function count = coloured_objects(img)
     % You should return the variable count as a 3-vector.
     % count(1) should be the number of red objects, count(2) the number of green objects, count(3) the number of blue objects
     
+count = [0 0 0];
+
 [H,S,V] = rgb2hsv(img);
 imshow(H);
 count = [0 0 0];
 
 blue = H > 0.6;
-green = H > 0.25 & H < 0.6;
-red = H < 0.1 & H > 0;
+green = H > 0.2 & H < 0.6;
+red = H < 0.2 & H > 0;
 
-stats = regionprops("table", blue);
+stats = regionprops("table", red);
 count(1) = length(stats.Centroid);
 stats = regionprops("table", green);
 count(2) = length(stats.Centroid);
-stats = regionprops("table", red);
+stats = regionprops("table", blue);
 count(3) = length(stats.Centroid);
 
 end
